a59f7fd37bc175f1fe268fa0d4a32232d8e071ac
cmanisha <cmanisha@gmail.com>
2017-12-26 21:00:10 +0000

    updated project

---
+++ devops-project-3-master1/devops-project-3-master/Numbers/src/devops/numbers/NumberWords.java
@@ -0,0 +1,22 @@
+package devops.numbers;
+
+public class NumberWords {
+
+	public String toWords( int number ) {
+		String result = "" ;
+		return result.trim() ;
+		
+		if number==1
+				then
+				{
+			 result = "one" ;
+		
+				}
+	else
+	{
+		result = "" ;
+	}
+	return result.trim() ;
+	}
+
+}
