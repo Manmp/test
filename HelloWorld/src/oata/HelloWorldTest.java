060c4700c551aa5277a840165c3a56dc16c6c38c
cmanisha <cmanisha@gmail.com>
2017-12-27 12:41:20 +0000

    Added HelloWorld

---
+++ HelloWorld/src/oata/HelloWorldTest.java
@@ -0,0 +1,31 @@
+
+/**-
+ * 
+ */
+package oata;
+
+import static org.junit.Assert.*;
+//
+import org.junit.Test;
+
+//import static org.junit.Assert.assertEquals;
+
+
+/**
+ * @author cmanisha
+ *
+ */
+public class HelloWorldTest {
+
+	/**
+	 * Test method for {@link oata.HelloWorld#HelloWorld()}.
+	 */
+	@Test
+	public void testHelloWorld() {
+		fail("Not yet implemented message");
+	}
+	@Test
+	public void testHelloWorld1() {
+		assertEquals("Pass test","1","1");
+	}
+}
